a0196e599eab11de80a2e0a8ed4ab1b22357cfe5
dehnert <dehnert@cs.rwth-aachen.de>
2017-08-11 12:02:09 +0200

    building target cocoalib

--- Formula/cocoa.rb
+++ Formula/cocoa.rb
@@ -14,7 +14,7 @@ class Cocoa < Formula
     system "mkdir", "#{prefix}/lib"
 
     system "./configure", "--threadsafe-hack", "--with-cxxflags=-Wno-deprecated-declarations -fPIC", "--prefix=#{prefix}"
-    system "make"
+    system "make", "cocoalib"
     system "make", "install"
 
     system "mv", "#{prefix}/include/CoCoA-#{version}", "#{prefix}/include/CoCoA"
