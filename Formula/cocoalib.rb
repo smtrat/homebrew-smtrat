dfcdf26ca7ff5bcbec224122976a1bc85bdccf14
dehnert <dehnert@cs.rwth-aachen.de>
2017-10-11 13:51:45 +0200

    addressing issue #1

--- Formula/cocoalib.rb
+++ Formula/cocoalib.rb
@@ -9,6 +9,11 @@ class Cocoalib < Formula
   depends_on "gmp" => ["c++11"]
 
   def install
+    // Fix for old version of CoCoALib that refers to tr1 headers.
+    for file in ["Lexer", "Main", "LineProviders"]
+        inreplace "src/CoCoa-5/#{file}.C", "boost/tr1/memory.hpp","memory"
+    end
+
     system "mkdir", "#{prefix}/include"
     system "mkdir", "#{prefix}/lib"
 
