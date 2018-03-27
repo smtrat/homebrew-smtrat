c26073088f9582e838480fbb55ecf103aebd0058
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2018-03-27 11:56:21 +0200

    Fixed issues raised in brew audit

--- Formula/carl.rb
+++ Formula/carl.rb
@@ -1,5 +1,5 @@
 class Carl < Formula
-  desc "A Computer ARithmetic and Logic library."
+  desc "Computer ARithmetic and Logic library"
   homepage "https://github.com/smtrat/carl"
   url "https://github.com/smtrat/carl/archive/17.12.zip"
   version "17.12"
@@ -7,19 +7,16 @@ class Carl < Formula
 
   head "https://github.com/smtrat/carl.git", :using => :git
 
-  option "with-thread-safe", "Make CArL thread-safe."
-  option "with-cocoalib", "Build CArL with support for CoCoALib."
-  option "with-cln", "Build CArL with support for cln."
-  option "with-ginac", "Build CArL with support for GiNaC."
+  option "with-thread-safe", "Build with thread-safe support"
 
   depends_on :macos => :mavericks
   depends_on "cmake"
-  depends_on "boost" => ["c++11"]
-  depends_on "gmp" => ["c++11"]
+  depends_on "boost"
+  depends_on "gmp"
   depends_on "eigen"
-  depends_on "cln" if build.with?("cln")
-  depends_on "ginac" if build.with?("ginac")
-  depends_on "moves-rwth/misc/cocoalib" if build.with?("cocoalib")
+  depends_on "cln" => :optional
+  depends_on "ginac" => :optional
+  depends_on "moves-rwth/misc/cocoalib" => :optional
 
   def install
     args = %w[
@@ -29,7 +26,7 @@ class Carl < Formula
     args << "-DTHREAD_SAFE=ON" if build.with?("thread-safe")
     args << "-DUSE_CLN_NUMBERS=ON" if build.with?("cln")
     args << "-DUSE_GINAC=ON" if build.with?("ginac")
-    args << "-DUSE_COCOA=ON" if build.with?("cocoa")
+    args << "-DUSE_COCOA=ON" if build.with?("cocoalib")
 
     mktemp do
       system "cmake", buildpath, *(std_cmake_args + args)
