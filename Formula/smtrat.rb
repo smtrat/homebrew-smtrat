9d104dfe67851de5a542fc6951a368e6bb34984b
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-06-11 11:13:38 +0200

    Bumping version.

--- Formula/smtrat.rb
+++ Formula/smtrat.rb
@@ -1,9 +1,9 @@
 class Smtrat < Formula
   desc "SMT Real Arithmetic Toolbox"
   homepage "https://github.com/smtrat/smtrat"
-  url "https://github.com/smtrat/smtrat/archive/v2.1.zip"
-  version "v2.1"
-  sha256 "5b65cd2d8f81e22ee13c8f875bf1fafa0b84d8478a8cae7592cb6b628618c963"
+  url "https://github.com/smtrat/smtrat/archive/v2.2.zip"
+  version "v2.2"
+  sha256 "738a7cfe82556ef3d7c27f37d4199339f64d54f95d169fd12e8c1b73dd467c8d"
 
   head "https://github.com/smtrat/smtrat.git", :using => :git
 
